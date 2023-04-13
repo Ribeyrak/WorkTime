0dcb6a1fd6c154dabe6fe1482a48108ed4cfffaa
Evhen Lukhtan <ev.lukhtan@gmail.com>
2023-04-13 16:16:54 +0300

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, '14.0'
+use_frameworks!
+
+target 'WorkTime' do
+ 
+pod 'SnapKit', '~> 5'
+pod 'Alamofire'
+pod 'Then'
+pod 'R.swift','~> 7'
+pod 'SwiftLint','~> 0'
+
+end
---
+++ WorkTime/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  WorkTime
+//
+//  Created by Evhen Lukhtan on 13.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .purple
+    }
+}
+
